a288125476977ddd2b7a3ae307b82ca094712525
Juan Carlos Merlos Albarracin <juan.merlosalbarracin@plexus.es>
2021-02-11 13:23:41 +0100

    index on develop: 384c0f6 fead: finish edit profile UI without interaction or funtionality

---
+++ Gitrepos/Screens and navigation flow/Edit Profile/TableViewScrollingController.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewScrollingController.swift
+//  Gitrepos
+//
+//  Created by Juan Carlos Merlos Albarracin on 11/2/21.
+//  Copyright © 2021 Swift En Profundidad. All rights reserved.
+//
+
+import Foundation
